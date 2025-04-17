5f030f02cf80fd7ba97a831977c29d74480296d3
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-04-17 21:06:23 +0200

    Removed unused code

--- src/backend/interpreter.c
+++ src/backend/interpreter.c
@@ -4,8 +4,7 @@
 #include "stack.h"
 #include "operator/interpreter.h"
 
-void interprete_operator(Stack* stack, token_t token);
-void interprete_literal_number(Stack* stack, token_t token);
+int interprete_literal_number(Stack* stack, token_t token);
 
 void interprete(token_t tokens[], int len) {
     Stack stack;
@@ -21,8 +20,7 @@ void interprete(token_t tokens[], int len) {
                 res = operator_interprete(&stack, token);
                 break;
             case LITERAL_NUMBER:
-                interprete_literal_number(&stack, token);
-                res = 0;
+                res = interprete_literal_number(&stack, token);
                 break;
         }
 
@@ -39,63 +37,7 @@ void interprete(token_t tokens[], int len) {
     stack_deinit(&stack);
 }
 
-void interprete_operator(Stack* stack, token_t token) {
-    assert(token.type == OPERATOR);
-
-    int err;
-
-    StackItem rhs;
-    if ((err = stack_pop_type(stack, NUMBER, &rhs))) {
-        if (err == E_STACK_TYPE) {
-            printf("Operand error: operand type is invalid!\n");
-        } else if (err == E_STACK_EMPTY) {
-            printf("Operand error: not enough operands on stack!\n");
-        }
-
-        return;
-    }
-
-    StackItem lhs;
-    if ((err = stack_pop_type(stack, NUMBER, &lhs))) {
-        if (err == E_STACK_TYPE) {
-            printf("Operand error: operand type is invalid!\n");
-        } else if (err == E_STACK_EMPTY) {
-            printf("Operand error: not enough operands on stack!\n");
-        }
-
-        return;
-    }
-
-    StackItem res;
-    res.type = NUMBER;
-
-    switch (token.value.operator) {
-        case OPERATOR_ADD:
-            res.data.number = lhs.data.number + rhs.data.number;
-            break;
-        case OPERATOR_SUBTRACT:
-            res.data.number = lhs.data.number - rhs.data.number;
-            break;
-        case OPERATOR_MULTIPLY:
-            res.data.number = lhs.data.number * rhs.data.number;
-            break;
-        case OPERATOR_DIVIDE:
-            if (rhs.data.number == 0) {
-                printf("Operator error!\n");
-                return;
-            }
-            res.data.number = lhs.data.number / rhs.data.number;
-            break;
-        default:
-            printf("Operator error!\n");
-            return;
-            break;
-    }
-
-    stack_push(stack, res);
-}
-
-void interprete_literal_number(Stack* stack, token_t token) {
+int interprete_literal_number(Stack* stack, token_t token) {
     assert(token.type == LITERAL_NUMBER);
 
     StackItem item;
@@ -105,5 +47,7 @@ void interprete_literal_number(Stack* stack, token_t token) {
     if (stack_push(stack, item)) {
         printf("Stack error!\n");
     }
+
+    return 0;
 }
 
